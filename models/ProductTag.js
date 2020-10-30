c254ab1d5eaf455a6de7a601874f6c0a2bb6bd69
Cody Wyoming <68657852+npokamestov@users.noreply.github.com>
2020-10-30 15:45:36 -0400

    completed models

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -1,12 +1,33 @@
 const { Model, DataTypes } = require('sequelize');
 
 const sequelize = require('../config/connection');
+const { destroy } = require('./Product');
 
 class ProductTag extends Model {}
 
 ProductTag.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'product',
+        key: 'id'
+      }
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'tag',
+        key: 'id'
+      }
+    }
   },
   {
     sequelize,
