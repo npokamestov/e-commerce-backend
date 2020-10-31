ad3871a6031ca9f2e103a18a7e7326d2c9d20ee4
Cody Wyoming <68657852+npokamestov@users.noreply.github.com>
2020-10-30 22:04:15 -0400

    finshed api routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -10,6 +10,7 @@ router.get('/', (req, res) => {
       {
         model: Product,
         attributes: [
+          'id',
           'product_name',
           'price',
           'stock'
--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -7,12 +7,66 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  Product.findAll({
+    include: [
+      {
+        model: Category,
+        attributes: [
+          'id',
+          'category_name'
+        ]
+      },
+      {
+        model: Tag,
+        attributes: [
+          'id',
+          'tag_name'
+        ]
+      }
+    ]
+  }).then(dbProdData => res.json(dbProdData))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  Product.findOne({
+    where: {
+      id: req.params.id
+    },
+    include: [
+      {
+        model: Category,
+        attributes: [
+          'id',
+          'category_name'
+        ]
+      },
+      {
+        model: Tag,
+        attributes: [
+          'id',
+          'tag_name'
+        ]
+      }
+    ]
+  })
+  .then(dbProdData => {
+    if (!dbProdData) {
+      res.status(404).json({ message: 'No Product found with this ID' });
+      return;
+    }
+    res.json(dbProdData);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 // create new product
@@ -91,6 +145,22 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  Product.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(dbProdData => {
+    if (!dbProdData) {
+      res.status(404).json({ message: 'No Product found with this ID' });
+      return;
+    }
+    res.json(dbProdData);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 module.exports = router;
